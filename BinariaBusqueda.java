e34470763ef763dfe33676dcb0b10165286d698b
FV13 <43671750+fv13@users.noreply.github.com>
2019-02-06 20:03:27 +0100

    Update BinariaBusqueda.java

--- BinariaBusqueda.java
+++ BinariaBusqueda.java
@@ -1,24 +1,15 @@
 package unico;
-
 import javax.swing.JOptionPane;
-
 public class BinariaBusqueda {
-
 	public static void main(String[] args) {
 		
-		
 //	 BusquedaBimaria_Swing ventana = new 	 BusquedaBimaria_Swing();
-		
-	TablaBinaria test = new TablaBinaria(10);
+		TablaBinaria test = new TablaBinaria(10);
 	JOptionPane.showMessageDialog(null,test);
 test.sort();
 int num =  Integer.parseInt(JOptionPane.showInputDialog("Introduzca el numero que desea buscar  : "));
 JOptionPane.showMessageDialog(null,"Matriz ordenada:  "+ test);
 JOptionPane.showMessageDialog(null, " Posicion:  " + test.buscar(test.matriz, num));
-
-
-	
-	
 	}
 
 }
